f4852470a39f95d3ac69803dfce61ba01d275ed6
blabel3 <blakecabel@gmail.com>
2015-10-05 20:44:21 -0500

    Tested and debugged it. Yep.

--- ftc_app/FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/PopsicleDrive.java
+++ ftc_app/FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/PopsicleDrive.java
@@ -1,6 +1,7 @@
 package com.qualcomm.ftcrobotcontroller.opmodes;
 
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
 
 /**
  * Created by BAbel on 9/21/2015.
@@ -22,7 +23,7 @@ public class PopsicleDrive extends OpMode{
     @Override
     public void loop() {
         float leftY = -gamepad1.left_stick_y;
-        float rightY = -gamepad1.right_stick_stick_y;
+        float rightY = -gamepad1.right_stick_y;
 
         leftMotor.setPower(leftY);
         rightMotor.setPower(rightY);
